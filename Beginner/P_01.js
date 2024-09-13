140404f383460dc09a121ece28eba7dcf38ab2c8
Neko_Kami <162889885+Neko0kami@users.noreply.github.com>
2024-09-13 15:49:10 +0000

    01

---
+++ Beginner/P_01.js
@@ -0,0 +1,21 @@
+/* WAP to check two numbers and return true if the sum of the two numebers is 100*/
+
+const sumIs100 = function (a, b) {
+    if (a + b === 100) {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+const equal100 = (a,b) => (a === 100 || b === 100);
+
+console.log(equal100(0, 100))
+
+// Alternative
+
+const sum = (a,b) => (a === 100 || b === 100 || a+b === 100)
+
+console.log(sum(0,100));
+console.log(sum(100,0));
