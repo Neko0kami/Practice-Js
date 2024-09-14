722f3aeae1358087c2cb39a4624d21d413cdeaad
Neko_Kami <162889885+Neko0kami@users.noreply.github.com>
2024-09-14 09:28:18 +0000

    String SLice

--- Beginner/P_02.js
+++ Beginner/P_02.js
@@ -9,12 +9,6 @@ console.log(getExtension("haha.dat"));
 // console.log(str.lastIndexOf("."));
 // console.log(str.slice(4));
 
-const getExtension2 = (str) => {
-    const arr = str.split(".");
-    const str2 = arr[-1]
-    return str2
-}
 
-console.log(getExtension2("HAHA.Dat"));
 
 
