fa9e3295bfbcd7f22c3cb7af5138180472d0eec6
Neko_Kami <162889885+Neko0kami@users.noreply.github.com>
2024-09-14 09:22:41 +0000

    Dates

---
+++ Beginner/P_04.js
@@ -0,0 +1,24 @@
+/* 
+WAP to get the current date.
+Expected output: dd/mm/yyy , mm/dd/yyy or dd-mm-yyy , mm-dd-yyy
+*/
+
+
+let date = new Date()
+
+console.log(date.toDateString());
+console.log(date.toLocaleDateString());
+
+// Alternate 
+
+const formateDate = (date = new Date()) => {
+    const day = date.getDate()
+    const month = date.getMonth()+1
+    const year = date.getFullYear()
+
+    console.log(`${day}/${month}/${year}`);
+    
+}
+
+console.log(formateDate());
+
