3ce7defea268acb10a225ab648637de8b8a50e6d
Neko_Kami <162889885+Neko0kami@users.noreply.github.com>
2024-09-14 09:41:53 +0000

    05

---
+++ Beginner/P_05.js
@@ -0,0 +1,13 @@
+/* 
+WAP to insert "New!" before a string if not present already,
+if it's already there then do nothing
+*/
+
+const newStr = (str) => {if (str.indexOf("New!") === 0) {
+    return str;
+}else{
+    return `New! ${str}`;
+}}
+
+console.log(newStr("Hola"));
+console.log(newStr("New! Hola"));
