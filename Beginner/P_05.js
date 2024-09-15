e5044791629cdef3d153e16ea7b609b5439503c0
Neko_Kami <162889885+Neko0kami@users.noreply.github.com>
2024-09-15 05:16:53 +0000

    .indexOf

--- Beginner/P_05.js
+++ Beginner/P_05.js
@@ -5,9 +5,10 @@ if it's already there then do nothing
 
 const newStr = (str) => {if (str.indexOf("New!") === 0) {
     return str;
-}else{
+}
     return `New! ${str}`;
-}}
+}
 
 console.log(newStr("Hola"));
-// console.log(newStr("New! Hola"));
+console.log(newStr("New! Hola"));
+
